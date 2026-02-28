e07088961528931d32345c27742f79d403e7cc0d
Mateo Villa Rojo <villarojomateo@gmail.com>
2026-02-27 19:36:31 -0500

    🚧 Creo tablas automáticamente por el momento

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
-
+from app.database import Base, engine
+from app import models
 from app.routers import equipo, jugador, partido
 
 app = FastAPI(title="torneo api")
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
