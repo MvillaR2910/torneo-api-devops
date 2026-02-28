03604ce02e3672ccc16b6e9cf4c6e7a950bbb332
Mateo Villa Rojo <villarojomateo@gmail.com>
2026-02-27 23:25:08 -0500

    ✨ Preparo tests para la DB test (override)

---
+++ tests/conftest.py
@@ -0,0 +1,37 @@
+import os
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from fastapi.testclient import TestClient
+
+from app.database import Base, get_db
+from app.main import app
+
+TEST_DATABASE_URL = os.getenv("DATABASE_URL_TEST")
+
+engine_test = create_engine(TEST_DATABASE_URL)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine_test)
+
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture(scope="session", autouse=True)
+def prepare_test_database():
+    Base.metadata.drop_all(bind=engine_test)
+    Base.metadata.create_all(bind=engine_test)
+    yield
+    Base.metadata.drop_all(bind=engine_test)
+
+
+@pytest.fixture()
+def client():
+    app.dependency_overrides[get_db] = override_get_db
+    with TestClient(app) as c:
+        yield c
+    app.dependency_overrides.clear()
