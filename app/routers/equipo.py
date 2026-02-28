8259b8363980506ae481db50e59daea5e9b54929
Mateo Villa Rojo <villarojomateo@gmail.com>
2026-02-27 20:36:36 -0500

    ✨ Primer CRUD con equipos y(POST/GET)

--- app/routers/equipo.py
+++ app/routers/equipo.py
@@ -1,7 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app.database import get_db
+from app import models, schemas
 
 router = APIRouter()
 
-@router.get("/")
-def listar_equipos():
-    return []
+
+@router.post("/", response_model=schemas.EquipoOut)
+def crear_equipo(payload: schemas.EquipoCreate, db: Session = Depends(get_db)):
+    equipo = models.Equipo(
+        nombre=payload.nombre,
+        ciudad=payload.ciudad,
+        entrenador=payload.entrenador,
+    )
+    db.add(equipo)
+    db.commit()
+    db.refresh(equipo)
+    return equipo
+
+
+@router.get("/", response_model=list[schemas.EquipoOut])
+def listar_equipos(db: Session = Depends(get_db)):
+    equipos = db.query(models.Equipo).all()
+    return equipos
